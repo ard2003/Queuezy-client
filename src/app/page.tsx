24a7ab1c4fd8593b1869b195e313a17329af1715
arshad <arshadshibu45@gmail.com>
2025-01-25 10:59:04 +0530

    layout changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-   <>
-   
-   </>
+  <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden">
+    <div className=" max-w-7xl w-full">
+
+    </div>
+  </main>
   );
 }
