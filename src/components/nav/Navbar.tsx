523dc42c618d49fd06014c0e153a6225c12ca740
shemil-global <shemil@globalgov.ae>
2025-01-25 10:41:37 +0530

    updated

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-white md:py-5 md:px-36'>
+         <div className='bg-[#F6F7F8] h-24 text-black  md:flex md:justify-between items-center px-5 font-[graphik] rounded-2xl cursor-pointer '>
+         <p>QueEzzy</p>
+         <div className='flex items-center space-x-1 hover:underline'>
+        {/* <IoLocation /> */}
+        <p>Select Location</p>
+      </div>
+      <div className='relative flex items-center'>
+        <span className='absolute left-2 bg-yellow-300 rounded-full p-2'>
+          {/* <IoSearch className='text-gray-600' /> */}
+        </span>
+        <input
+          type="text"
+          className='pl-14 py-2 w-64 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400'
+          placeholder='Search your doctor'
+        />
+      </div>
+      <p className='hover:underline'>About</p>
+      <p className='hover:underline'>Help</p>
+      <div className='space-x-4 flex'>
+        <p className='px-4 py-2 rounded-3xl transition duration-300 hover:bg-blue-500 hover:text-white'>
+          Sign Up
+        </p>
+        <p className='px-4 py-2 rounded-3xl transition duration-300 hover:bg-blue-500 hover:text-white'>Login</p>
+      </div>
+
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
