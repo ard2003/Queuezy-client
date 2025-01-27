bdd542226ec53f8a3ae7a84e6d4c5a513b143ef1
arshad <arshadshibu45@gmail.com>
2025-01-27 10:40:13 +0530

    layout changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/sections/Hero";
 
 
 export default function Home() {
   return (
-  <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden">
+  <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className=" max-w-7xl w-full">
-
+      <Hero/>
     </div>
   </main>
   );
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div></div>
+    <div className='pb-20 pt-36  bg-[#252b61] font-sans rounded-[40px]'>
+      <div className='flex justify-center'>
+        <h1 className=' text-9xl font-bold text-[#dbebff]'>Healthcare</h1>
+      </div>
+    </div>
   )
 }
 
