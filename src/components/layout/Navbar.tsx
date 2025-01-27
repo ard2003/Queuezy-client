a2e69741a007c65ca8a99073c4f639105c2025e1
arshad <arshadshibu45@gmail.com>
2025-01-27 16:19:40 +0530

    updated

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 
 const Navbar = () => {
   return (
-    <div className='bg-white md:py-5 md:px-36'>
-         <div className='bg-[#F6F7F8] h-24 text-black  md:flex md:justify-between items-center px-5 font-[graphik] rounded-2xl cursor-pointer '>
+    <div className='bg-white md:py-5 md:px-36 font-sans'>
+         <div className='bg-[#F6F7F8] h-24 text-black  md:flex md:justify-between items-center px-5 font-semibold rounded-2xl cursor-pointer hidden '>
          <p>QueEzzy</p>
-         <div className='flex items-center space-x-1 hover:underline'>
-        {/* <IoLocation /> */}
+         <div className='flex items-center space-x-1 hover:underline text-[#667085]'>
+         <MdOutlineLocationOn />
         <p>Select Location</p>
       </div>
       <div className='relative flex items-center'>
-        <span className='absolute left-2 bg-yellow-300 rounded-full p-2'>
-          {/* <IoSearch className='text-gray-600' /> */}
+        <span className='absolute left-2 bg-yellow-300 rounded-full p-2 hover:bg-[#e7c2d4]'>
+        <CiSearch />
         </span>
         <input
           type="text"
-          className='pl-14 py-2 w-64 rounded-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400'
-          placeholder='Search your doctor'
+          className='pl-14 py-2 w-64 rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400 tracking-wide'
+          placeholder='Find your doctor'
         />
       </div>
       <p className='hover:underline'>About</p>
       <p className='hover:underline'>Help</p>
-      <div className='space-x-4 flex'>
+      <div className='space-x-4 flex bg-white rounded-full border-2 border-white shadow-sm'>
         <p className='px-4 py-2 rounded-3xl transition duration-300 hover:bg-blue-500 hover:text-white'>
           Sign Up
         </p>
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import { GoArrowRight } from "react-icons/go";
+import { MdOutlineBloodtype } from "react-icons/md";
+import { LuPill } from "react-icons/lu";
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36  bg-[#252b61] font-sans rounded-[40px]'>
-      <div className='flex justify-center'>
-        <h1 className=' text-9xl font-bold text-[#dbebff]'>Healthcare</h1>
+    <div className='bg-[#252b61] font-sans rounded-[40px] p-10 relative overflow-clip' >
+
+      <div className='flex justify-center flex-col gap-4'>
+        <div className='flex justify-center'>
+        <h1 className=' lg:text-[200px] text-6xl font-extrabold text-[#dbebff] tracking-normal'>Healthcare</h1>
+
+        </div>
+      <div className=' flex justify-around flex-row'>
+        <div className=' flex flex-row gap-2 '>
+          <div className='flex items-center'>
+            <div className=' rounded-full bg-[#e7c2d4] text-2xl  lg:text-4xl p-1'><MdOutlineBloodtype/></div>
+          </div>
+          <div><p className=' text-3xl text-white'>Reduce HbA1c</p></div>
+        </div>
+        <div className=' flex flex-row gap-2 '>
+          <div className='flex items-center'>
+            <div className=' rounded-full bg-[#a3dac2] text-2xl  lg:text-4xl p-1 '><LuPill /></div>
+          </div>
+          <div><p className=' text-3xl text-white '>No more medications</p></div>
+        </div>
+        </div>
       </div>
+      
+    <div className='flex justify-center h-full w-full pt-36'>
+    <Image src='/HeroDocter.jpg' alt='docterCutout' width={430} height={441} className='absolute  bottom-0 hidden lg:block'/>
+    </div>
+    <div className=' flex flex-col justify-between bottom-2 md:flex-row space-y-3 '>
+        <div className='max-w-[330px] text-white font-medium'><p>IF  YOU&apos;RE LOOKING FOR A CREATIVE AND EASY WAY TO BUILD A WEBSITE, WOW! IS THE PERFECT SOLUTION.</p></div>
+        <div className=' flex flex-row align-bottom  '>
+        <div className=' rounded-full bg-[#e7c2d4] flex items-center justify-center w-14 h-14 text-2xl'><GoArrowRight/></div>
+        <div className=' flex justify-center bg-[#e7c2d4] h-14 items-center p-5 rounded-full font-medium'><h5>Book Consultation</h5></div>
+        <div className=' rounded-full bg-[#e7c2d4] flex items-center justify-center w-14 h-14 text-2xl'><GoArrowRight/></div>
+        
+        </div>
+      </div >
     </div>
   )
 }
